31d599c1f4d05e9e96e1d35a20929324a2692b65
Shuji <marcellalter@gmail.com>
2024-07-08 13:35:47 +0200

    it worky

---
+++ src/main/java/me/shuji/Disenchanting/Handlers/DisenchantHandler.java
@@ -0,0 +1,77 @@
+package me.shuji.Disenchanting.Handlers;
+
+import me.shuji.Disenchanting.Utils.Functions;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class DisenchantHandler {
+
+	public Map<UUID, Enchantment> removeLastEnchant = new HashMap<>();
+	public Map<UUID, Boolean> isModified = new HashMap<>();
+
+	private ItemStack fI, sI;
+
+	public DisenchantHandler(PrepareAnvilEvent e, ItemStack fItem, ItemStack sItem) {
+		if (e != null) {
+			fI = fItem;
+			sI = sItem;
+			if (fI != null && sI != null)
+				if (sI.getType() == Material.BOOK) {
+					if (fItem.getType() == Material.ENCHANTED_BOOK) HandleEnchantedBook(e);
+					else HandleDisenchant(e);
+				}
+		}
+	}
+
+	public void HandleEnchantedBook(PrepareAnvilEvent e) {
+		EnchantmentStorageMeta meta = (EnchantmentStorageMeta) fI.getItemMeta();
+		Object[] enchs = meta.getStoredEnchants().keySet().toArray();
+		if (enchs.length <= 1)
+			return;
+		Enchantment removeEnchant = (Enchantment) enchs[enchs.length - 1];
+		for (Object i : enchs) {
+			Enchantment ench = (Enchantment) i;
+			if (e.getInventory().getRenameText().equalsIgnoreCase(Functions.GetEnchantmentName(ench)))
+				removeEnchant = ench;
+		}
+		ItemStack resultBook = new ItemStack(Material.ENCHANTED_BOOK);
+		EnchantmentStorageMeta resultMeta = (EnchantmentStorageMeta) resultBook.getItemMeta();
+		resultMeta.addStoredEnchant(removeEnchant, meta.getStoredEnchantLevel(removeEnchant), true);
+		resultBook.setItemMeta(resultMeta);
+		meta.removeStoredEnchant(removeEnchant);
+		for (HumanEntity he : e.getViewers()) {
+			removeLastEnchant.put(he.getUniqueId(), removeEnchant);
+		}
+		e.setResult(resultBook);
+		for (HumanEntity he : e.getViewers())
+			isModified.put(he.getUniqueId(), true);
+
+	}
+
+	public void HandleDisenchant(PrepareAnvilEvent e) {
+		Object[] enchs = fI.getItemMeta().getEnchants().keySet().toArray();
+		if (enchs.length == 0)
+			return;
+		Enchantment removeEnchant = (Enchantment) enchs[enchs.length - 1];
+
+		ItemStack resultBook = new ItemStack(Material.ENCHANTED_BOOK);
+		EnchantmentStorageMeta resultMeta = (EnchantmentStorageMeta) resultBook.getItemMeta();
+
+		resultMeta.addStoredEnchant(removeEnchant, fI.getItemMeta().getEnchantLevel(removeEnchant), true);
+		for (HumanEntity he : e.getViewers()) {
+			removeLastEnchant.put(he.getUniqueId(), removeEnchant);
+		}
+		resultBook.setItemMeta(resultMeta);
+		e.setResult(resultBook);
+		for (HumanEntity he : e.getViewers())
+			isModified.put(he.getUniqueId(), true);
+	}
+}
--- src/main/java/me/shuji/Disenchanting/Main.java
+++ src/main/java/me/shuji/Disenchanting/Main.java
@@ -1,31 +1,96 @@
 package me.shuji.Disenchanting;
 
+import me.shuji.Disenchanting.Handlers.DisenchantHandler;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.inventory.AnvilInventory;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Map;
 import java.util.logging.Logger;
 
 
 public final class Main extends JavaPlugin implements Listener {
 
 	FileConfiguration config = getConfig();
-	Logger console = getLogger();
-	ItemStack firstItem = null;
-	Map.Entry<Enchantment, Integer> enchToMove = null;
+	public static Logger console;
+	private static DisenchantHandler dh;
 
 	@Override
 	public void onEnable() {
 		setConfig();
-
+		console = getLogger();
 		if (config.getBoolean("enabled")) {
 			onDisable();
 		}
 
 		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPrepareAnvil(PrepareAnvilEvent e) {
+		dh = new DisenchantHandler(e, e.getInventory().getFirstItem(), e.getInventory().getSecondItem());
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onInventoryClick(InventoryClickEvent e) {
+		if (!e.isCancelled()) {
+			HumanEntity ent = e.getWhoClicked();
+			if (ent == null)
+				return;
+
+			Player player = (Player) ent;
+			Inventory inv = e.getInventory();
+
+			if (inv instanceof AnvilInventory) {
+				AnvilInventory ainv = (AnvilInventory) inv;
+				InventoryView view = e.getView();
+				ItemStack currentItem = e.getCurrentItem();
+				if (e.getSlotType() == InventoryType.SlotType.RESULT && currentItem != null && currentItem.getType() != Material.AIR) {
+					if (inv.getContents()[1] != null) {
+						if (inv.getContents()[1].getType() == Material.BOOK) {
+							ItemStack firstItem = inv.getContents()[0];
+							int amount = inv.getContents()[1].getAmount();
+							ItemStack bookItem = inv.getContents()[1];
+							view.setCursor(ainv.getResult()); //give player the disenchanted book
+							bookItem.setAmount(amount - 1); //remove one book
+
+							//region Remove enchant
+							if (dh.removeLastEnchant.containsKey(ent.getUniqueId())) {
+								if (dh.removeLastEnchant.get(ent.getUniqueId()) != null) {
+									if (firstItem.getType() == Material.ENCHANTED_BOOK) {
+										EnchantmentStorageMeta firstMeta = (EnchantmentStorageMeta) firstItem.getItemMeta();
+										Enchantment enchant = dh.removeLastEnchant.get(ent.getUniqueId());
+										firstMeta.removeStoredEnchant(enchant);
+										firstItem.setItemMeta(firstMeta);
+									} else {
+										Enchantment removeEnch = dh.removeLastEnchant.get(ent.getUniqueId());
+										firstItem.removeEnchantment(removeEnch);
+									}
+									for (HumanEntity he : e.getClickedInventory().getViewers())
+										dh.removeLastEnchant.put(he.getUniqueId(), null);
+								}
+							}
+							//endregion
+
+							inv.setContents(new ItemStack[]{firstItem, bookItem, null});
+						}
+					}
+				}
+			}
+		}
 	}
 
 	private void setConfig() {
